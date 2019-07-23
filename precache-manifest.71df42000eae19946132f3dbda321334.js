6317708f0648c135c45375734c693856bfa96a87
moonfalljade <moonfalljade@hotmail.com>
2019-07-23 13:29:44 +0800

    Updates

---
+++ precache-manifest.71df42000eae19946132f3dbda321334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0df393bc744e4ccd26a15a09874f409",
+    "url": "/moonfallmaple/student-profile/index.html"
+  },
+  {
+    "revision": "b7b23a388c4722c2b923",
+    "url": "/moonfallmaple/student-profile/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "a3ebc125fc3e486acade",
+    "url": "/moonfallmaple/student-profile/static/css/main.3aca3135.chunk.css"
+  },
+  {
+    "revision": "b7b23a388c4722c2b923",
+    "url": "/moonfallmaple/student-profile/static/js/2.a9e2383e.chunk.js"
+  },
+  {
+    "revision": "a3ebc125fc3e486acade",
+    "url": "/moonfallmaple/student-profile/static/js/main.1b4f52b5.chunk.js"
+  },
+  {
+    "revision": "366b16f5efdaf5759bcd",
+    "url": "/moonfallmaple/student-profile/static/js/runtime~main.e2a50aa9.js"
+  }
+]);
